f4ebe0321069b50185394798c241350c66a8643b
Hydr4-mc <owensmeyer1@gmail.com>
2025-08-30 16:25:36 -0500

    Update ShieldStatus.java for 1.21.7

--- src/main/java/walksy/shieldstatus/main/ShieldStatus.java
+++ src/main/java/walksy/shieldstatus/main/ShieldStatus.java
@@ -3,25 +3,25 @@ package walksy.shieldstatus.main;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.client.command.v2.ClientCommandManager;
 import net.fabricmc.fabric.api.client.command.v2.ClientCommandRegistrationCallback;
-import net.minecraft.client.MinecraftClient;
 import net.minecraft.text.Text;
 import walksy.shieldstatus.manager.ConfigManager;
 import com.mojang.brigadier.arguments.IntegerArgumentType;
 
-
 public class ShieldStatus implements ModInitializer {
     @Override
-    public void onInitialize()
-    {
+    public void onInitialize() {
         ConfigManager.load();
 
-        ClientCommandRegistrationCallback.EVENT.register((dispatcher, registryAccess) ->
+        // Command registration may have changed in newer APIs.
+        ClientCommandRegistrationCallback.EVENT.register((dispatcher, registryAccess) -> 
             dispatcher.register(ClientCommandManager.literal("walksyshieldstatusopacity")
                 .then(ClientCommandManager.argument("num", IntegerArgumentType.integer())
                     .executes(context -> {
-                        ConfigManager.enabledOpacity = IntegerArgumentType.getInteger(context, "num");
-                        ConfigManager.disabledOpacity = IntegerArgumentType.getInteger(context, "num");
-                        context.getSource().sendFeedback(Text.of("Opacity set to: [Enabled] " + ConfigManager.enabledOpacity + " [Disabled] " + ConfigManager.disabledOpacity));
+                        int value = IntegerArgumentType.getInteger(context, "num");
+                        ConfigManager.enabledOpacity = value;
+                        ConfigManager.disabledOpacity = value;
+                        // Text.of is still valid in 1.21.x, but always check for API updates
+                        context.getSource().sendFeedback(Text.literal("Opacity set to: [Enabled] " + value + " [Disabled] " + value));
                         ConfigManager.save();
                         return 1;
                     })
